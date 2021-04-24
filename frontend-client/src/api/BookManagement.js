6390298d0d2a3894815d3afa64ac57ce62d4ef75
Kien <mrkienkptn@gmail.com>
2021-04-25 00:15:35 +0700

    require api

--- frontend-client/src/api/BookManagement.js
+++ frontend-client/src/api/BookManagement.js
@@ -6,3 +6,12 @@ export const searchBook = (keyword="", year="", category="", author="", page=1)=
 }
     
 export const getBorrowingBooks = (userId, page=1)=> get(`/get-borrowing-book?userId=${userId}&page=${page}`, {})
+
+/**
+ * cần thêm những api sau
+ * + lấy thông tin cụ thể của 1 quyển sách, tham số: bookId (get)
+ * + thêm sách vào mục yêu thích, tham số: bookId, userId (post)
+ * + lấy danh sách sách đã yêu thích, tham số : userId, page (get)
+ * + đăng ký mượn sách, tham số: userId, bookId (post)
+ * + gia hạn sách, tham số: userId, bookId, newExpireTime (post)
+ */
