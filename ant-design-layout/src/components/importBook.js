1986840cdd47591ff7920db9aa292ebfb84cc15d
TruongTD1112 <truong.td173429@sis.hust.edu.vn>
2021-04-29 12:46:23 +0700

    create folder axios api

---
+++ ant-design-layout/src/components/importBook.js
@@ -0,0 +1,51 @@
+import React, {useEffect, useState} from 'react';
+import PropTypes from "prop-types";
+import {
+  Button,
+  Input,
+  InputNumber,
+  Popconfirm,
+  Space,
+  Table,
+  Tag,
+  Modal,
+  Form,
+  Column,
+  Select,
+} from "antd";
+class table extends React.Component{
+const dataSource = [
+    {
+      key: '1',
+      name: 'Mike',
+      age: 32,
+      address: '10 Downing Street',
+    },
+    {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+    },
+  ];
+  
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
+  
+<Table dataSource={dataSource} columns={columns} />;
+}
