78fcdbade97b3afb22b1f8390893c73e132c7b55
Vu Khac Chinh <vukhacchinh99@gmail.com>
2021-05-27 14:07:24 +0700

    c

--- Backend/routes/Book_element.js
+++ Backend/routes/Book_element.js
@@ -74,7 +74,7 @@ router.get('/getBookRent', async(req, res) =>{
 router.post('/retreiveBook', async(req, res) => {
     try{
         const {bookId, userId} = req.body;
-        await book_element.findByIdAndUpdate(bookId, {status: "free", user: null});
+        await book_element.findByIdAndUpdate(bookId, {status: "f", user: null});
         await book.findByIdAndUpdate(userId, {$pull: {borrowBooks: {bookElementId : bookId}}})
     }
     catch(err){
@@ -84,11 +84,47 @@ router.post('/retreiveBook', async(req, res) => {
 
 router.get('/getDataRegister', async(req, res) =>{
     try{
-        const listBook = await book_element.find({status: "pending", user : {$ne : null, $exists: true}}).populate('user');
+        const listBook = await book_element.find({status: "pend", user : {$ne : null, $exists: true}}).populate('user');
         res.json(listBook);
     }
     catch(err){
         res.status(200).json({message : err.message});
     }
+})
+
+router.get('/getinforbook', async (req, res) => {
+    try{
+        const {bookID} = req.body;
+        let InforBookElement = await book_element.findOne({_id: bookID});
+        if(InforBookElement == null) return null;
+        else {
+            let InforBook = await book.findById(InforBookElement.rootBook);
+            if(InforBookElement.status == "pending") res.json("Sasch da duoc thue");
+            else if (InforBookElement.status == "rent") res.json("sach da duoc thue");
+            else {
+                let data = {
+                    "title" : InforBook.title,
+                    "genre" : InforBook.genre,
+                    "author" : InforBook.author,
+                    "status" : InforBookElement.status
+                }
+                res.json(data);
+            }
+        }
+    }
+    catch(err) {
+        res.status(400).json(null);
+    }
+	
+	router.post('/getbookRent', async(req, res) =>{
+		// lay danh sach sach da thue
+    try{
+        const listBook = await book_element.find({status: "rent"}).populate('user');
+        res.status(200).json(listBook);
+    }
+    catch(err){
+        res.json({message: err.message});
+    }
+})
 })
 module.exports = router;
